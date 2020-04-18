97d78546f403ba261c1dfc1e9a40c777725c91ca
Gonzalo Knoll <knollgonzalo@gmail.com>
2020-04-17 21:28:49 -0300

    Resolución puntos 1 - 5

---
+++ Practica clase 4/Logica/Extensiones.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logica
+{
+    public static class Extensiones
+    {
+        public static int RetornarCantidadPalabras(this string cadena)
+        {
+            string[] arraypalabras = cadena.Split(' ');
+            return arraypalabras.Count();
+        }
+
+        public static int RetornarCantidadPalabras(this string cadena, char separador)
+        {
+            string[] arraypalabras = cadena.Split(separador);
+            return arraypalabras.Count();
+        }
+
+        public static decimal ObtenerMinutosEntreFechas(this DateTime fecha1, DateTime fecha2)
+        {
+            return Convert.ToDecimal((fecha1 - fecha2).TotalSeconds) / 60;
+        }
+
+        public static double ObtenerPromedio(this List<int> listanumeros)
+        {
+            return listanumeros.Sum() / listanumeros.Count();
+        }
+
+        public static int ObtenerMultiplicaciónParesImpares(this List<int> listanumeros)
+        {
+            return listanumeros.FindAll(x => x % 2 == 0).Sum() * (listanumeros.First(x => x % 2 != 0) * 2 + listanumeros.FindAll(x => x % 2 != 0).Sum() * (-1));
+        }
+    }
+}
---
+++ Practica clase 4/Practica clase 4/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada con un ensamblado.
+[assembly: AssemblyTitle("Practica clase 4")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Practica clase 4")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("721ecfc7-2c84-4491-81e9-54be05fd16a0")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+// Puede especificar todos los valores o utilizar los números de compilación y de revisión predeterminados
+// mediante el carácter '*', como se muestra a continuación:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
